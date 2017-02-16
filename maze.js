b50a9cbec7e316e8a916d0e6f2bc233c99914b9e
IronHKristy <kristynhorton85@gmail.com>
2017-02-15 19:30:05 -0500

    wed 7pm

--- maze.js
+++ maze.js
@@ -6,7 +6,7 @@ var board = [
   ['wall',   'path', 'wall', 'wall', 'path', 'wall', 'path', 'path', 'path', 'path', 'path', 'path', 'wall', 'path', 'wall', 'wall', 'path', 'wall' ],//row 3
   ['wall',   'path', 'path', 'wall', 'path', 'path', 'path', 'wall', 'path', 'path', 'wall', 'path', 'path', 'path', 'wall', 'path', 'path', 'wall' ],//row 4
   ['wall',   'wall', 'path', 'wall', 'wall', 'path', 'wall', 'path', 'path', 'path', 'path', 'wall', 'path', 'wall', 'wall', 'path', 'wall', 'wall' ],//row 5
-  ['pacman', 'path', 'path', 'path', 'path', 'path', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'path', 'path', 'path', 'path', 'path', 'path' ],//row 6
+  ['pizza', 'path', 'path', 'path', 'path', 'path', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'path', 'path', 'path', 'path', 'path', 'path' ],//row 6
   ['wall',   'wall', 'path', 'wall', 'wall', 'path', 'path', 'path', 'path', 'path', 'path', 'path', 'path', 'wall', 'wall', 'path', 'wall', 'wall' ],//row 7
   ['wall',   'wall', 'path', 'wall', 'wall', 'wall', 'wall', 'wall', 'path', 'path', 'wall', 'wall', 'wall', 'wall', 'wall', 'path', 'wall', 'wall' ],//row 8
   ['wall',   'path', 'path', 'path', 'wall', 'wall', 'path', 'path', 'path', 'path', 'path', 'path', 'wall', 'wall', 'path', 'path', 'path', 'wall' ],//row 9
@@ -30,8 +30,8 @@ function renderBoard () {
         gridClass = 'wall';
       } else if (grid === 'path') {
         gridClass = 'path';
-      } else if (grid === 'pacman') {
-        gridClass = 'pacman';
+      } else if (grid === 'pizza') {
+        gridClass = 'pizza';
       } else if (grid === 'eSign1') {
         gridClass = 'eSign1';
       } else if (grid === 'eSign2') {
@@ -51,37 +51,37 @@ function renderBoard () {
 
 
 
-//find pacman and return his position
+//find pizza and return his position
 var rowA;
-var colB;
+var colA;
 
-function findTwo() {
+function findPizza() {
 board.forEach(function (row, rowIndex) {
   row.forEach(function (col, colIndex) {
-    if(col === 'pacman') {
+    if(col === 'pizza') {
       rowA = rowIndex;
-      colB = colIndex;
+      colA = colIndex;
     }
   });
   });
-return rowA, colB;
+return rowA, colA;
 }
 
 
 //find nizar and return his position
-var rowC;
-var colD;
+var rowB;
+var colB;
 
 function findNizar() {
 board.forEach(function (row, rowIndex) {
   row.forEach(function (col, colIndex) {
     if(col === 'nizar') {
-      rowC = rowIndex;
-      colD = colIndex;
+      rowB = rowIndex;
+      colB = colIndex;
     }
   });
 });
-  return rowC, colD;
+  return rowB, colB;
 }
 
 // move nizar based on random number generator
@@ -90,7 +90,7 @@ board.forEach(function (row, rowIndex) {
 function moveNizar() {
   findNizar();
   // console.log(randomDir);
-  if (rowC === undefined || colD === undefined) {
+  if (rowB === undefined || colB === undefined) {
     return;
   }
 
@@ -99,16 +99,16 @@ function moveNizar() {
 
   switch(randomDir) {
     case 0:
-      moveUp(rowC, colD, 'nizar');
+      moveUp(rowB, colB, 'nizar');
       break;
     case 1:
-      moveDown(rowC, colD, 'nizar');
+      moveDown(rowB, colB, 'nizar');
       break;
     case 2:
-      moveLeft(rowC, colD, 'nizar');
+      moveLeft(rowB, colB, 'nizar');
       break;
     case 3:
-      moveRight(rowC, colD, 'nizar');
+      moveRight(rowB, colB, 'nizar');
       break;
   }
   return;
@@ -124,7 +124,7 @@ function moveLeft(row, col, character) {
     board[row][col - 1] = character;
     board[row][col] = 'path';
   } else if
-    (character === 'pacman' && board[row][col - 1] === 'eSign1') {
+    (character === 'pizza' && board[row][col - 1] === 'eSign1') {
       board[5][8] = character;
       board[1].splice(1, 1, "nizar");
 
@@ -178,19 +178,19 @@ $(document).keydown(function(ev) {
       return;
     }
     ev.preventDefault();
-    findTwo();
+    findPizza();
     switch(ev.keyCode) {
       case 38:
-        moveUp(rowA, colB, 'pacman');
+        moveUp(rowA, colA, 'pizza');
         break;
       case 40:
-        moveDown(rowA, colB, 'pacman');
+        moveDown(rowA, colA, 'pizza');
         break;
       case 37:
-        moveLeft(rowA, colB, 'pacman');
+        moveLeft(rowA, colA, 'pizza');
         break;
       case 39:
-        moveRight(rowA, colB, 'pacman');
+        moveRight(rowA, colA, 'pizza');
         break;
     }
 
